e76f5490fb056acdfd5bdbfa1177370bc6c87ec5
Manhduc13 <manhduc130702@gmail.com>
2024-06-11 11:04:38 +0700

    Add authorization

---
+++ src/main/java/com/example/identityservice/configuration/ApplicationInitConfig.java
@@ -0,0 +1,40 @@
+package com.example.identityservice.configuration;
+
+import com.example.identityservice.entity.User;
+import com.example.identityservice.enums.Role;
+import com.example.identityservice.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+@Configuration
+public class ApplicationInitConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(ApplicationInitConfig.class);
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    ApplicationRunner applicationRunner (UserRepository userRepository){
+        return args -> {
+            if(userRepository.findByUsername("admin").isEmpty()){
+                PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+                HashSet<String> roles = new HashSet<>();
+                roles.add(Role.ADMIN.name());
+                User user = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .roles(roles)
+                        .build();
+                userRepository.save(user);
+                log.warn("An admin account has been created with password: admin");
+            }
+        };
+    }
+}
