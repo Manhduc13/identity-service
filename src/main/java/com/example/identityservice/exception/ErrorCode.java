83c5105e2aad758c4c49efd69f7ff11336d1c8f6
Manhduc13 <manhduc130702@gmail.com>
2024-06-12 11:12:38 +0700

    Handling exception with HttpCode

--- src/main/java/com/example/identityservice/exception/ErrorCode.java
+++ src/main/java/com/example/identityservice/exception/ErrorCode.java
@@ -1,36 +1,30 @@
 package com.example.identityservice.exception;
 
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+
+@Getter
 public enum ErrorCode {
 
-    UNCATEGORIZED_EXCEPTION(9999,"Uncategorized error"),
-    ENUMKEY_INVALID(1000,"Invalid enum key"),
-    USERNAME_EXISTED(1001,"Username has been taken"),
-    USERNAME_INVALID(1002,"Username must be at least 3 characters"),
-    PASSWORD_INVALID(1003,"Password must be at least 6 characters."),
-    USER_NOT_FOUND(1004,"User not found!"),
-    UNAUTHENTICATED(1005, "Unauthenticated")
+    UNCATEGORIZED_EXCEPTION(9999, HttpStatus.INTERNAL_SERVER_ERROR,"Uncategorized error"),
+    ENUMKEY_INVALID(1000, HttpStatus.BAD_REQUEST,"Invalid enum key"),
+    USERNAME_EXISTED(1001, HttpStatus.BAD_REQUEST,"Username has been taken"),
+    USERNAME_INVALID(1002, HttpStatus.BAD_REQUEST,"Username must be at least 3 characters"),
+    PASSWORD_INVALID(1003, HttpStatus.BAD_REQUEST,"Password must be at least 6 characters."),
+    USER_NOT_FOUND(1004, HttpStatus.NOT_FOUND,"User not found!"),
+    UNAUTHENTICATED(1005, HttpStatus.UNAUTHORIZED,"Unauthenticated"),
+    UNAUTHORIZED(1006,HttpStatus.FORBIDDEN,"You do not have permission")
     ;
+
     private int code;
+    private HttpStatusCode statusCode;
     private String message;
 
-    ErrorCode(int code, String message) {
+    ErrorCode(int code,HttpStatusCode statusCode, String message) {
         this.code = code;
+        this.statusCode = statusCode;
         this.message = message;
     }
 
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
