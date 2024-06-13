9b82f22581e2638b59d90e6643c0b48e41023955
Manhduc13 <manhduc130702@gmail.com>
2024-06-13 11:31:11 +0700

    Add role and permission

--- src/main/java/com/example/identityservice/enums/Role.java
+++ src/main/java/com/example/identityservice/enums/Role.java
@@ -4,3 +4,6 @@ public enum Role {
     ADMIN,
     USER
 }
+// Each role has different permission (privilege)
+// 1 user -> many role and 1 role -> many user || many - many
+// 1 role -> permission and 1 permission -> many role || many - many
--- src/main/java/com/example/identityservice/service/UserService.java
+++ src/main/java/com/example/identityservice/service/UserService.java
@@ -14,11 +14,9 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
